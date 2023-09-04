a6fd51bf57ae5c3b9ebd6a09b54bb3d33e68af12
이창건 <114966864+geon0430@users.noreply.github.com>
2023-09-05 02:24:43 +0900

    Add files via upload

--- run.sh
+++ run.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
+# TensorFlow가 필요로 하는 버전에 맞춰 NumPy를 설치
 pip install numpy==1.22
-pip install opencv-python==4.5.3.56
-pip install tensorflow==2.12.0
-pip install matplotlib
-pip install imageio
-pip install imgaug
-pip install scikit-learn
-pip install keras
-pip install pydot
 
+# OpenCV를 설치
+pip install opencv-python==4.5.3.56
+
+# TensorFlow를 설치
+pip install tensorflow==2.12.0
+
+# Matplotlib를 설치
+pip install matplotlib
+
+# libGL 라이브러리를 설치 (Linux)
 apt-get update
 apt-get install -y libgl1-mesa-glx
+
+# 또는 headless 버전으로 설치할 수도 있음 (Linux)
 apt-get install -y libgl1-mesa-glx-headless
-apt-get install -y git
+
+apt-get install -y vim
